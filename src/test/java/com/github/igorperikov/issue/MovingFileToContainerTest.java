10b791f468defd4d3ae2a490f035c815fe33fcff
IgorPerikov <perikov.igor@gmail.com>
2018-03-03 07:31:29 +0300

    apply not-an-issue patch

--- src/test/java/com/github/igorperikov/issue/MovingFileToContainerTest.java
+++ src/test/java/com/github/igorperikov/issue/MovingFileToContainerTest.java
@@ -1,6 +1,10 @@
 package com.github.igorperikov.issue;
 
+import com.github.dockerjava.api.DockerClient;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
@@ -9,9 +13,17 @@ public class MovingFileToContainerTest {
             .withClasspathResourceMapping("myfile", "/tmp/myfile", BindMode.READ_ONLY);
 
     @Test
-    public void justToMakeADebugPoint() {
+    public void justToMakeADebugPoint() throws Exception {
         container.start();
         System.out.println("I'm up");
-        container.stop();
+
+        DockerClient client = DockerClientFactory.instance().client();
+
+        try (TarArchiveInputStream tar = new TarArchiveInputStream(client.copyArchiveFromContainerCmd(container.getContainerId(), "/tmp/myfile").exec())) {
+            tar.getNextEntry();
+            String content = IOUtils.toString(tar);
+
+            assert content.startsWith("this\nis");
+        }
     }
 }
